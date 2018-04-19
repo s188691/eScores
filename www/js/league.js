0a587c412ba8e7a71095c8483e84180976cd22ed
yungyazy <wiktor.wnekowicz@gmail.com>
2018-04-19 22:53:07 +0200

    api thing

--- www/js/league.js
+++ www/js/league.js
@@ -1,12 +1,37 @@
 var api = 'https://api.pandascore.co/';
 var game = 'videogames';
 var apiKey = '?token=psPK5jHHbwepozs5tpNXkthrjpsqJYjqRJaLGgdF6mgoQw1SX5g';
+var url = api + game + apiKey;
 
-function setup() {
-	var url = api + game + apiKey;
-	loadJSON(url, gotData);
-}
+//function setup() {
+//	var url = api + game + apiKey;
+//	loadJSON(url, gotData);
+//}
 	
-function gotData(data) {
-	println(data);
+//function gotData(data) {
+//	println(data);
+//}
+var gameContainer = document.getElementById("over");
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", function() {
+	
+var ourRequest = new XMLHttpRequest();
+	ourRequest.open('GET', url);
+	ourRequest.onload = function() {
+		var ourData = JSON.parse(ourRequest.responseText);
+		renderHTML(ourData);
+		};
+	ourRequest.send();
+
+});
+
+function renderHTML(data) {
+	var htmlString = "";
+	
+	for (i = 0; i < data.length; i++) {
+		htmlString += "<p>" + data[i].name + ".</p>";
+	}
+	
+	gameContainer.insertAdjacentHTML('beforeend', htmlString);
 }
