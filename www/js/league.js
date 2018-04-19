2fd0b175a0a23f50ea981a86d1ba3df1f61c039b
yungyazy <wiktor.wnekowicz@gmail.com>
2018-04-19 22:07:18 +0200

    api

--- www/js/league.js
+++ www/js/league.js
@@ -5,17 +5,8 @@ var apiKey = '?token=psPK5jHHbwepozs5tpNXkthrjpsqJYjqRJaLGgdF6mgoQw1SX5g';
 function setup() {
 	var url = api + game + apiKey;
 	loadJSON(url, gotData);
+}
 	
-function gotData(
-
-
-
-
-function UserAction() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "https://api.pandascore.co/lol/champions.json?token=psPK5jHHbwepozs5tpNXkthrjpsqJYjqRJaLGgdF6mgoQw1SX5g", true);
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send();
-    var response = JSON.parse(xhttp.responseText);
-	document.getElementById("liga").innerHTML = response;
+function gotData(data) {
+	println(data);
 }
