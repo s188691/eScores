07e0328e5f5e929cbe75b0485058f47b39c19b08
yungyazy <wiktor.wnekowicz@gmail.com>
2018-04-19 21:02:07 +0200

    error handle

--- www/js/google.js
+++ www/js/google.js
@@ -17,6 +17,24 @@ function google_login_in(){
 		  // The firebase.auth.AuthCredential type that was used.
 		  var credential = error.credential;
 		  // ...
-		});  
+		}); 
+
+firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a Google Access Token.
+    // You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+});
+
+
+		
     }
 
