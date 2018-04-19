c0069aa059a38d414145435d6ebc48af10077178
yungyazy <wiktor.wnekowicz@gmail.com>
2018-04-19 17:19:56 +0200

    p5 add

--- www/js/league.js
+++ www/js/league.js
@@ -1,3 +1,16 @@
+var api = 'https://api.pandascore.co/';
+var game = 'videogames';
+var apiKey = '?token=psPK5jHHbwepozs5tpNXkthrjpsqJYjqRJaLGgdF6mgoQw1SX5g';
+
+function setup() {
+	var url = api + game + apiKey;
+	loadJSON(url, gotData);
+	
+function gotData(
+
+
+
+
 function UserAction() {
     var xhttp = new XMLHttpRequest();
     xhttp.open("POST", "https://api.pandascore.co/lol/champions.json?token=psPK5jHHbwepozs5tpNXkthrjpsqJYjqRJaLGgdF6mgoQw1SX5g", true);
