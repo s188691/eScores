cb6417d67efd903f88dfed4c497406cdafe3a052
sandalyjudasza <kamil2000-12@o2.pl>
2018-05-09 19:06:10 +0200

    stare logowanie fb
    Signed-off-by: sandalyjudasza <kamil2000-12@o2.pl>

--- www/js/facebook.js
+++ www/js/facebook.js
@@ -1,49 +1,40 @@
-var fbprovider = new firebase.auth.FacebookAuthProvider();
+window.fbAsyncInit = function() {
 
-function facebook_login_in(){
-        
-		firebase.auth().signInWithRedirect(fbprovider).then(function(result) {
-		  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-		  var token = result.credential.accessToken;
-		  // The signed-in user info.
-		  var user = result.user;
-		  // ...
-		}).catch(function(error) {
-		  // Handle Errors here.
-		  var errorCode = error.code;
-		  var errorMessage = error.message;
-		  // The email of the user's account used.
-		  var email = error.email;
-		  // The firebase.auth.AuthCredential type that was used.
-		  var credential = error.credential;
-		  // ...
-		});
 
-firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-   
-    var token = result.credential.accessToken;
-  
-    var user = result.user;
-  
-  }
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  alert(errorMessage);
+FB.init({
+
+      appId      : 'myappid', // App ID
+      channelUrl : '//myurl/fb./en/us.html', // Channel File
+      status     : true, // check login status
+      cookie     : true, // enable cookies to allow the server to access the session
+      xfbml      : true  // parse XFBML
+    });
+
+
+FB.getLoginStatus(function(response) {
+alert("Response");
+alert("getloginstatus");
+    if (response.status === 'connected') {
+        // connected
+    } else if (response.status === 'not_authorized') {
+        // not_authorized
+        login();
+    } else {
+        // not_logged_in
+        login();
+    }
 });
 
+};
 
-		
-}
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/pl_PL/sdk.js#xfbml=1&version=v3.0&appId=2145472378801355&autoLogAppEvents=1';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));                       
 
+  (function(d){
+     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement('script'); js.id = id; js.async = true;
+     js.src = "//connect.facebook.net/en_US/all.js";
+     ref.parentNode.insertBefore(js, ref);
+     alert("Lodaed SDK");
+   }(document));
 
 
